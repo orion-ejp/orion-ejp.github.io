4521fd871976f2a23c4f623f8eaee724d2148b51
TarGue <taras.guenther@protonmail.com>
2019-05-21 09:14:10 +0200

    integrate open modal by id via url.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,12 @@
+$(document).ready(function() {
 
+for (i = 0; i< 1000;i++){
 
-
-
-
+    if(window.location.href.indexOf('#'+i+'term') != -1) {
+        $('#'+i+'term').modal('show');
+      }
+}
+    
+  
+  });
+  
